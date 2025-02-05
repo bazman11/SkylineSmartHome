22f4dad67c0455efc3e6f46aba112e1127948dca
bazman11 <bazman1@etf.unsa.ba>
2025-02-05 23:49:22 +0100

    Added device class and its interface. Now focus on inheritance

---
+++ SkylineSmartHome/Interfaces/IDevice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkylineSmartHome.Interfaces
+{
+    public interface IDevice
+    {
+        int GetId();
+        void SetId(int id);
+
+        string GetName();
+        void SetName(string name);
+
+        bool GetStatus();
+        void SetStatus(bool status);
+
+        void TurnOn();
+        void TurnOff();
+        string GetStatusString();
+    }
+
+}
--- SkylineSmartHome/Program.cs
+++ SkylineSmartHome/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Dobrodošli u Skyline Smart Home sistem.");
+
+int input;
+
+do
+{
+    Console.WriteLine("-1 za izlaz");
+    input = int.Parse(Console.ReadLine());
+} while (input != -1);
